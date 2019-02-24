fc39b505f2602ea0883ee4dde4203e7a0eb7cd84
edvin palm <edpalm@outlook.com>
2019-02-24 16:27:57 +0100

    get /catches with queries, Fix: Dynamic pages and NextPage

--- routes/catches.js
+++ routes/catches.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const router = require('express').Router()
-
+require('dotenv').config()
 const Catch = require('../models/CatchSchema')
 
 router.route('/catches/')// unsafe protect with jwt.
@@ -15,28 +15,78 @@ router.route('/catches/')// unsafe protect with jwt.
       imageUrl: request.body.imageUrl,
       time: request.body.time
     })
-    await newCatch.save(function (err, data) {
+    await newCatch.save(function (err, catchData) {
       if (err) {
         console.log(err)
       }
       response.status(201).json({
-        _self: `http://localhost:3000/catches/${data._id}`,
-        user: data.user,
-        position: data.position,
-        species: data.species,
-        weight: data.weight,
-        length: data.length,
-        imageUrl: data.imageUrl,
-        time: data.time
+        links: {
+          self: `${process.env.HOST_URL}${request.url}/${catchData._id}`
+        },
+        data: {
+          type: 'catches',
+          id: catchData._id,
+          attributes: {
+            user: catchData.user,
+            position: catchData.position,
+            species: catchData.species,
+            weight: catchData.weight,
+            length: catchData.length,
+            imageUrl: catchData.imageUrl,
+            time: catchData.time
+          }
+        }
       })
       // call webhook emit payload function!.
     })
   })
   .get(async (request, response, next) => {
-    if (!request.query.page) {
-      console.log('first page')
-    } else {
-      console.log('not first page')
+    try {
+      let page = 0
+      let nextPage = 2
+      // let resourcesPerPage = 10
+
+      if (request.query.page) {
+        page = parseInt(request.query.page, 10)
+        nextPage = page + 1
+      }
+
+      let catchData = await Catch.find({}).exec()
+
+      let catchArray = []
+      /* let startIndex = page * resourcesPerPage
+      let endIndex = startIndex + (resourcesPerPage - 1) */
+      let startIndex = 0
+      let endIndex = 2
+
+      for (let i = startIndex; i < endIndex; i++) {
+        console.log(i)
+        console.log(catchData[i]._id)
+        let catchResource = {
+          links: {
+            self: `${process.env.host_URL}/catches/${catchData[i]._id}`
+          },
+          id: catchData[i]._id,
+          user: catchData[i].user,
+          position: catchData[i].position,
+          species: catchData[i].species,
+          weight: catchData[i].weight,
+          length: catchData[i].length,
+          imageUrl: catchData[i].imageUrl,
+          time: catchData[i].time
+        }
+        catchArray.push(catchResource)
+      }
+      response.status(200).json({
+        links: {
+          self: `${process.env.HOST_URL}${request.url}`,
+          next: `${process.env.HOST_URL}/catches?page=${nextPage}`
+        },
+        data: catchArray
+      })
+    } catch (err) {
+      console.log(err)
+      next()
     }
   })
   // return a set of X amount of catches
@@ -47,12 +97,24 @@ router.route('/catches/:id')
     try {
       let catchData = await Catch.findById(request.params.id).exec()
       if (catchData !== null) {
-        response.status(200).json(catchData)
+        response.status(200).json({
+          links: {
+            self: `${process.env.HOST_URL}${request.url}`
+          },
+          data: {
+            id: catchData._id,
+            user: catchData.user,
+            position: catchData.position,
+            species: catchData.species,
+            weight: catchData.weight,
+            length: catchData.length,
+            imageUrl: catchData.imageUrl,
+            time: catchData.time
+          }
+        })
       } else {
         next()
       }
-      // hateoas browsing links?
-      // message?
     } catch (err) {
       next()
     }
@@ -62,7 +124,22 @@ router.route('/catches/:id')
       let updatedCatch = await Catch.findOneAndUpdate(request.params.id, request.body).exec()
       if (updatedCatch !== null) {
         let updatedCatch = await Catch.findById(request.params.id).exec()
-        response.status(200).json(updatedCatch)
+        response.status(200).json({
+          links: {
+            self: `${process.env.HOST_URL}${request.url}`
+          },
+          data: {
+            type: 'catches',
+            id: updatedCatch._id,
+            user: updatedCatch.user,
+            position: updatedCatch.position,
+            species: updatedCatch.species,
+            weight: updatedCatch.weight,
+            length: updatedCatch.length,
+            imageUrl: updatedCatch.imageUrl,
+            time: updatedCatch.time
+          }
+        })
       } else {
         next()
       }
@@ -72,11 +149,24 @@ router.route('/catches/:id')
   })
   .patch(async (request, response, next) => { // unsafe, protect with jwt. Redundant since .put?
     try {
-      console.log(request.body)
       let updatedCatch = await Catch.findOneAndUpdate(request.params.id, request.body).exec()
       if (updatedCatch !== null) {
         let updatedCatch = await Catch.findById(request.params.id).exec()
-        response.status(200).json(updatedCatch)
+        response.status(200).json({
+          links: {
+            self: `${process.env.HOST_URL}${request.url}`
+          },
+          data: {
+            id: updatedCatch._id,
+            user: updatedCatch.user,
+            position: updatedCatch.position,
+            species: updatedCatch.species,
+            weight: updatedCatch.weight,
+            length: updatedCatch.length,
+            imageUrl: updatedCatch.imageUrl,
+            time: updatedCatch.time
+          }
+        })
       } else {
         next()
       }
@@ -85,7 +175,6 @@ router.route('/catches/:id')
     }
   })
   .delete(async (request, response, next) => { // unsafe, protect with jwt
-    console.log(request.params.id)
     try {
       let deletedCatch = await Catch.findByIdAndDelete(request.params.id).exec()
       if (deletedCatch !== null) {
